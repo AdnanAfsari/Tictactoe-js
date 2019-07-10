5d337241611ac9c3c7a3c89965aca3dd89106d21
Adnan Afsari <aarahul1991@gmail.com>
2019-07-10 16:24:19 +0600

    minimax algo and bg image added

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,6 +111,56 @@ const game = (() => {
   }
 
 
+  const minimax = (newBoard, player) => {
+  	var availSpots = emptySquares();
+
+  	if (checkWin(newBoard, gameBoard.huPlayer)) {
+  		return {score: -10};
+  	} else if (checkWin(newBoard, gameBoard.aiPlayer)) {
+  		return {score: 10};
+  	} else if (availSpots.length === 0) {
+  		return {score: 0};
+  	}
+  	var moves = [];
+  	for (var i = 0; i < availSpots.length; i++) {
+  		var move = {};
+  		move.index = newBoard[availSpots[i]];
+  		newBoard[availSpots[i]] = player;
+
+  		if (player == gameBoard.aiPlayer) {
+  			var result = minimax(newBoard, gameBoard.huPlayer);
+  			move.score = result.score;
+  		} else {
+  			var result = minimax(newBoard, gameBoard.aiPlayer);
+  			move.score = result.score;
+  		}
+
+  		newBoard[availSpots[i]] = move.index;
+
+  		moves.push(move);
+  	}
+
+  	var bestMove;
+  	if(player === gameBoard.aiPlayer) {
+  		var bestScore = -10000;
+  		for(var i = 0; i < moves.length; i++) {
+  			if (moves[i].score > bestScore) {
+  				bestScore = moves[i].score;
+  				bestMove = i;
+  			}
+  		}
+  	} else {
+  		var bestScore = 10000;
+  		for(var i = 0; i < moves.length; i++) {
+  			if (moves[i].score < bestScore) {
+  				bestScore = moves[i].score;
+  				bestMove = i;
+  			}
+  		}
+  	}
+
+  	return moves[bestMove];
+  }
 
   return {
     startGame
