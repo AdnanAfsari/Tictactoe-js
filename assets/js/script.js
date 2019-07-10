3d102f6dd3e41d6ad3a81b0de6ae41e18113709f
James Okunlade <james.okunlade@gmail.com>
2019-07-10 11:09:59 +0100

    checkWin fucntion added

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,6 +60,20 @@ const game = (() => {
   }
 
 
+  const checkWin = (board, player) => {
+    let plays = board.reduce((a, e, i) =>
+    (e === player) ? a.concat(i) : a, []);
+    let gameWon = null;
+    for (let [index, win] of gameBoard.winCombos.entries()) {
+      if (win.every(elem => plays.indexOf(elem) > -1)) {
+        gameWon = {index: index, player: player};
+        break;
+      }
+    }
+    return gameWon;
+  }
+
+
 
 
   return {
